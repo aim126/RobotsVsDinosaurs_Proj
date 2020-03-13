286cfb74d3198aeb53e5d40f02dff6f21b889bbd
unknown <aim126@yahoo.com>
2020-03-13 14:19:07 -0500

    init'

---
+++ RobotsVSDinosaurs/Dinosaur.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RobotsVSDinosaurs
+{
+    class Dinosaur
+    {
+        //Member Variables
+        public string health;
+        public string energy;
+        public string attackpower ;
+
+    }
+
+
+
+
+}
---
+++ RobotsVSDinosaurs/Robot.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RobotsVSDinosaurs
+{
+    class Robot
+        
+    {
+        //Member Variables
+        public string name;
+        public string health;
+        public string powerlevel ;
+        public string Weapon;
+        public string attackpower; 
+     }
+
+        //Constarctor
+        public robot ()
+        {
+           name = "roby the robot";
+           health = "100 points" ;
+           powerlevel  = "10 bars " ;
+           Weapon = Sword ;
+           attackpower = "strike hard"
+
+
+        }
+         
+}
